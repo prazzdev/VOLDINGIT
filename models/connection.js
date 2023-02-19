29adb113bb387ac0cc50a26ae6d2d24668fb17fb
prazzdev <prazzid31@gmail.com>
2023-02-19 18:13:20 +0700

    modified:   models/connection.js

--- models/connection.js
+++ models/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: "bsrvkootqh4bl9quhuws-mysql.services.clever-cloud.com",
     user: "ubzqcgm9dofipdel",
     password: "zeVNe3NzwKeU9zo4Ddyg",
-    database: "bsrvkoepiz_31497959_kfmotqh4bl9quhuws"
+    database: "bsrvkootqh4bl9quhuws"
 })
 
 module.exports = db
